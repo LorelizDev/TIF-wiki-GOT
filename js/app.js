d5141ab0cf878de3b242028e3eb5c7b06b051ab5
[Rami154] <[ramirofernandez154@gmail.com]>
2023-05-22 20:20:38 -0300

    validacion form

--- js/app.js
+++ js/app.js
@@ -14,3 +14,40 @@ punto.forEach( ( cadaPunto, i )=> {
         punto[i].classList.add('activo')
     })
 })
+
+//Validacion formulario
+const nombre = document.getElementById('name')
+const email = document.getElementById('email')
+const asunto = document.getElementById('subject')
+const mensaje = document.getElementById('comentarios')
+const form = document.getElementById('form')
+const parrafo = document.getElementById('warnings')
+
+form.addEventListener('submit', e=>{
+    e.preventDefault()
+    let warnings = ''
+    let entrar = false
+    parrafo.innerHTML = ''
+    let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/
+    if(nombre.value.length < 4){
+        warnings += `El nombre no es valido <br>`
+        entrar = true
+    }
+    if(!regexEmail.test(email.value)){
+        warnings += `El email no es valido <br>`
+        entrar = true
+    }
+    if(asunto.value.length < 4){
+        warnings += `El asunto no es valido <br>`
+        entrar = true
+    }
+    if(mensaje.value.length < 4){
+        warnings += `El mensaje no es valido <br>`
+        entrar = true
+    }
+    if(entrar){
+        parrafo.innerHTML = warnings
+    } else {
+        parrafo.innerHTML = 'Enviado'
+    }
+})
