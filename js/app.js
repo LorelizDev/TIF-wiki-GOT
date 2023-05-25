ab8cd32f152e378b0a35f96f69158fc93e351a40
[Rami154] <[ramirofernandez154@gmail.com]>
2023-05-25 16:20:14 -0300

    eliminacion carrusel

--- js/app.js
+++ js/app.js
@@ -1,20 +1,3 @@
-// Funcionalidad del carrusel
-const grande = document.querySelector('.grande')
-const punto = document.querySelectorAll('.punto')
-
-punto.forEach( ( cadaPunto, i )=> {
-    punto[i].addEventListener('click',()=>{
-        let posicion = i
-        let operacion = posicion * -25
-        grande.style.transform = `translateX(${ operacion }%)`
-
-        punto.forEach(( cadaPunto, i )=> {
-            punto[i].classList.remove('activo')
-        })
-        punto[i].classList.add('activo')
-    })
-})
-
 //Validacion formulario
 const nombre = document.getElementById('name')
 const email = document.getElementById('email')
