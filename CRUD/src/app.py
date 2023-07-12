33d0f9500f4f18a70282776aefbf5ebe66605263
LorelizDev <loreliz.dev@gmail.com>
2023-07-12 15:53:13 +0200

    actualizacion de app.py

--- CRUD/src/app.py
+++ CRUD/src/app.py
@@ -8,7 +8,7 @@ CORS(app) #modulo cors es para que me permita acceder desde el frontend al backe
 
 
 # configuro la base de datos, con el nombre el usuario y la clave
-app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://LorelizDev:290531.nosotras@LorelizDev.mysql.pythonanywhere-services.com/LorelizDev$dbGot"
+app.config['SQLALCHEMY_DATABASE_URI']="mysql+pymysql://password@LorelizDev.mysql.pythonanywhere-services.com/LorelizDev$dbGot"
 # URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
 
